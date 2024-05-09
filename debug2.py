c1e7e72a1b941e35dfdede40742c579f65ed92e0
Ves Velchev <98955964+vesv@users.noreply.github.com>
2024-05-09 19:21:32 +0100

    Update debug2.py

--- debug2.py
+++ debug2.py
@@ -7,9 +7,9 @@ coords = np.array([[10, 5, 15, 6, 0],
                     [4, 4, 13, 6, 1],
                     [6, 5, 13, 16, 1]])
 
-flipped_coords = np.flip(np.flipud(coords))
-flipped_coords = np.flipud(flipped_coords)
-print(flipped_coords)
+flipped_coords = np.flip(np.flipud(coords)) #flip the order of items in the individual lists in the array
+flipped_coords = np.flipud(flipped_coords) #flip the order of the lists in the array
+print(flipped_coords) #should print the flipped array
 
 #the first issue is that numpy was imported after it was defined
 #after that i used numpy to first flip the order of items in the individal lists in the array
